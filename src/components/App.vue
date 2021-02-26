edf2dce0ff3cb468313116549799aa41b22a0039
Andrii Korzh <andriii.korzh@gmail.com>
2021-02-26 15:11:08 +0200

    Fix rates auto-update

--- src/components/App.vue
+++ src/components/App.vue
@@ -161,7 +161,9 @@
     },
     mounted () {
       // Setup rate update interval
-      differenceInHours(this.rates_updated_at, new Date) && this.updateRates()
+      if (!this.rates_updated_at || differenceInHours(this.rates_updated_at, new Date)) {
+        this.updateRates()
+      }
       ratesUpdateInterval = setInterval(() => this.updateRates(), 36e5)
 
       // Make input auto-focused
