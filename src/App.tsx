7238ff0b08eb470a65c3fee37e371c870d0651b0
Andrii Korzh <Andrii.Korzh@gmail.com>
2023-01-08 18:49:43 +0200

    remove 1000EUR and up hint

--- src/App.tsx
+++ src/App.tsx
@@ -159,29 +159,6 @@ export default function App() {
           </dd>
         </dl>
       </Show>
-      <Show when={state.value * getRate(state.currency, 'EUR') > 1000}>
-        <div class="text-center mt-6">
-          <svg
-            class="w-20 h-20 mb-4 inline stroke-orange-500"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-            ></path>
-          </svg>
-          <p class="dark:text-gray-400">
-            Вартість вашої посилки перевищує {state.currency === 'EUR' ? '' : 'еквівалент'}{' '}
-            <Currency value="1000" currency="EUR" toggle={['EUR', state.currency]} />, і чорт його
-            знає, як проходить митне оформлення такий вантаж
-          </p>
-        </div>
-      </Show>
       <Show when={state.value && !taxesTotal()}>
         <div class="text-center mt-6">
           <svg
